e310d320d22a1f42e931214ab98106bd912306f4
Lawrence <macknever@gmail.com>
2024-03-30 14:02:02 -0700

    Added dataloader spring module
    Added a simple module, and added the application properties yaml file

---
+++ data-loader-spring/src/main/java/com/lawrence/dataloaderspring/DataLoaderSpringApplication.java
@@ -0,0 +1,13 @@
+package com.lawrence.dataloaderspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataLoaderSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataLoaderSpringApplication.class, args);
+	}
+
+}
---
+++ data-loader-spring/src/test/java/com/lawrence/dataloaderspring/DataLoaderSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawrence.dataloaderspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataLoaderSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
