52e4c97aff7b1b19e692adf314fd1df27879ab4b
macknever <macknever@gmail.com>
2024-03-30 11:31:06 -0700

    Cleaned up everything

--- src/main/java/com/demo/ConditionFactoryDemo.java
+++
@@ -1,9 +0,0 @@
-package com.demo;
-
-import org.awaitility.Awaitility;
-import org.awaitility.core.ConditionFactory;
-
-public class ConditionFactoryDemo {
-    ConditionFactory waitCondition = Awaitility.await();
-}
-
