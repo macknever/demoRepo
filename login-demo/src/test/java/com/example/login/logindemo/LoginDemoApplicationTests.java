5c11f9479cbd1da65890c66e064e771ca35e1c5a
Lawrence <macknever@gmail.com>
2024-04-14 21:40:20 -0700

    Added log in page

---
+++ login-demo/src/test/java/com/example/login/logindemo/LoginDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.login.logindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
