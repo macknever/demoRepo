f66ceb7e5cf74417b5e4baa07bee3bda7b1d6642
Lawrence <macknever@gmail.com>
2024-04-16 07:59:51 -0700

    Added more exercises for stream

--- exercises/src/main/java/com/lawrence/corejava/stream/StreamExercises.java
+++ exercises/src/main/java/com/lawrence/corejava/stream/StreamExercises.java
@@ -1,9 +1,10 @@
 package com.lawrence.corejava.stream;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamExercises {
-
 
     public double average(List<Integer> nums) {
         return nums.stream().mapToInt(num -> num).average().orElse(0.0d);
@@ -13,7 +14,13 @@ public class StreamExercises {
         return strings.stream().map(String::toUpperCase).toList();
     }
 
+    public Map<Boolean, Integer> sumOfEvenAndOdd(List<Integer> nums) {
+        return nums.stream()
+                .collect(Collectors.partitioningBy(x -> x % 2 == 0, Collectors.summingInt(Integer::intValue)));
+    }
 
-
+    public <T> List<T> removeDuplicate(List<T> list) {
+        return list.stream().distinct().toList();
+    }
 
 }
--- exercises/src/test/java/com/lawrence/stream/StreamExercisesTest.java
+++ exercises/src/test/java/com/lawrence/stream/StreamExercisesTest.java
@@ -1,12 +1,15 @@
 package com.lawrence.stream;
 
-import com.lawrence.corejava.stream.StreamExercises;
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-import java.util.stream.Stream;
+import com.lawrence.corejava.stream.StreamExercises;
 
 class StreamExercisesTest {
     private StreamExercises exercises;
@@ -18,17 +21,32 @@ class StreamExercisesTest {
 
     @Test
     void averageShouldWork() {
-        List<Integer> nums = Stream.iterate(1, n -> n+1).limit(100).toList();
+        List<Integer> nums = Stream.iterate(1, n -> n + 1).limit(100).toList();
         double expect = 50.5d;
         double actual = exercises.average(nums);
-        Assertions.assertEquals(expect, actual);
+        assertEquals(expect, actual);
     }
 
     @Test
     void toUpperCaseShouldWork() {
-        List<String> strings = Stream.of("qwe","asd","zxc","qaz").toList();
+        List<String> strings = Stream.of("qwe", "asd", "zxc", "qaz").toList();
         List<String> actualStr = exercises.toUpperCase(strings);
-        Assertions.assertEquals("QWE", actualStr.get(0));
+        assertEquals("QWE", actualStr.get(0));
+    }
+
+    @Test
+    void sumOfEvenAndOddShouldWork() {
+        List<Integer> nums = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).toList();
+        Map<Boolean, Integer> sums = exercises.sumOfEvenAndOdd(nums);
+        assertEquals(30, sums.get(true));
+        assertEquals(25, sums.get(false));
+    }
+
+    @Test
+    void removeDuplicatedShouldWork() {
+        List<String> list = Stream.of("qwe", "qwe", "qwe", "ewq").toList();
+        List<String> distinctList = exercises.removeDuplicate(list);
+        assertEquals(2, distinctList.size());
     }
 
 }
