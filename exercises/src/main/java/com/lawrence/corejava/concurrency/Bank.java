db3b25e05e35f0c0b60eec6a9356ba8f805b8e95
Lawrence <macknever@gmail.com>
2024-05-23 06:38:11 -0700

    Finished Bank Class

--- exercises/src/main/java/com/lawrence/corejava/concurrency/Bank.java
+++ exercises/src/main/java/com/lawrence/corejava/concurrency/Bank.java
@@ -17,8 +17,29 @@ public class Bank {
     }
 
     public void transfer(int from, int to, double amount) {
+        if (accounts[from] < amount) {
+            return;
+        }
+        System.out.print(Thread.currentThread());
+
+        accounts[from] -= amount;
+        System.out.printf(" %10.2f from %d to %d", amount, from, to);
+
+        accounts[to] += amount;
+        System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
     }
 
-    public void getTotalBalance() {
+    public double getTotalBalance() {
+        double sum = 0.0d;
+
+        for (double a : accounts) {
+            sum += a;
+        }
+
+        return sum;
+    }
+
+    public int size() {
+        return accounts.length;
     }
 }
