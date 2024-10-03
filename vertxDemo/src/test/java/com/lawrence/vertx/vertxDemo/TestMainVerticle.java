4048c22c55f8903b1821a18b4580237d54d1486e
Lawrence <macknever@gmail.com>
2024-10-03 09:34:37 -0700

    Added Vert.X examples

--- vertxDemo/src/test/java/com/lawrence/vertx/vertxDemo/TestMainVerticle.java
+++ vertxDemo/src/test/java/com/lawrence/vertx/vertxDemo/TestMainVerticle.java
@@ -11,13 +11,13 @@ import io.vertx.junit5.VertxTestContext;
 @ExtendWith(VertxExtension.class)
 public class TestMainVerticle {
 
-  @BeforeEach
-  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
-    vertx.deployVerticle(new MainVerticle()).onComplete(testContext.succeeding(id -> testContext.completeNow()));
-  }
+    @BeforeEach
+    void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
+        vertx.deployVerticle(new MainVerticle()).onComplete(testContext.succeeding(id -> testContext.completeNow()));
+    }
 
-  @Test
-  void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
-    testContext.completeNow();
-  }
+    @Test
+    void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
+        testContext.completeNow();
+    }
 }
