c9dc5a8e3c4aaba3274a1e3c3fc1ff4ff382eca2
Lawrence Li <lawrence.li@globalrelay.net>
2025-01-23 09:38:22 -0800

    Updated consumer

--- springApp/src/main/java/com/lawrence/springApp/service/MessageConsumerService.java
+++ springApp/src/main/java/com/lawrence/springApp/service/MessageConsumerService.java
@@ -1,11 +1,31 @@
 package com.lawrence.springApp.service;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.lawrence.springApp.model.Author;
 import com.lawrence.springApp.model.AuthorRepository;
 
+@Service
 public class MessageConsumerService {
+    private static final Logger LOG = LoggerFactory.getLogger(MessageConsumerService.class);
     private final AuthorRepository authorRepository;
 
     public MessageConsumerService(final AuthorRepository authorRepository) {
         this.authorRepository = authorRepository;
     }
+
+//    @KafkaListener(topics = "${app.kafka.topic}", groupId = "${spring.kafka.consumer.group-id}")
+//    public void listenMessages(ConsumerRecord<String, String> record) {
+//
+//        authorRepository.save(author);
+//        LOG.info("Messeage stored, {}", author);
+//    }
+//
+//    private Author convertToAuthor(ConsumerRecord<String, String> record) {
+//        record.value().get
+//    }
 }
